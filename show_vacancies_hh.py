dd5f78645d3b161708a7643133b49eec30945b8e
PaSeRouS <paser96@mail.ru>
2022-02-17 22:09:00 +0300

    Исправление замечаний

--- show_vacancies_hh.py
+++ show_vacancies_hh.py
@@ -1,6 +1,47 @@
 import requests
 
 from mapping_result import display_table_of_result
+from predict_rub_salary import predict_rub_salary
+
+
+def get_request_hh(language, url):
+	vacancies_params = {
+		'text' : language,
+		'area' : 1, # Москва
+		'period' : 30
+	}
+
+	response = requests.get(url, params=vacancies_params)
+	response.raise_for_status()
+
+	return response
+
+
+def download_pages(response, language, url):
+	if response.json()['found'] > 100:
+		page = 0
+		pages_number = 1
+
+		vacancies = []
+
+		while page < pages_number:
+			page_params = {
+				'text' : language,
+				'area' : 1, # Москва
+				'period' : 30,
+				'page': page
+			}
+
+			page_response = requests.get(url, params=page_params)
+			page_response.raise_for_status()
+
+			pages_number = page_response.json()['pages']
+			page_data = page_response.json()['items']
+			page += 1
+	
+			vacancies.append(page_data)
+
+	return vacancies
 
 
 def show_hh_vacancies_info():
@@ -8,19 +49,19 @@ def show_hh_vacancies_info():
 
 	languages_of_programming = [
 		'TypeScript',
-		'Swift',
-		'Scala',
-		'Objective-C',
-		'Shell',
-		'Go',
-		'C',
-		'C#',
-		'C++',
-		'PHP',
-		'Ruby',
-		'Python',
-		'Java',
-		'JavaScript'
+		# 'Swift',
+		# 'Scala',
+		# 'Objective-C',
+		# 'Shell',
+		# 'Go',
+		# 'C',
+		# 'C#',
+		# 'C++',
+		# 'PHP',
+		# 'Ruby',
+		# 'Python',
+		# 'Java',
+		# 'JavaScript'
 	]
 
 	vacancies_for_language = {}
@@ -31,61 +72,25 @@ def show_hh_vacancies_info():
 		vacancies_processed = 0
 		sum_salary = 0
 
-		vacancies_params = {
-			'text' : language,
-			'area' : 1, # Москва
-			'period' : 30
-		}
+		response_hh = get_request_hh(language, vacancies_url)
+		vacancies = download_pages(response_hh, language, vacancies_url)
 
-		response = requests.get(vacancies_url, params=vacancies_params)
-		response.raise_for_status()
+		for page in vacancies:
+			for vacancy in page:
+				if vacancy['salary'] and vacancy['salary']['currency'] == 'RUR':
+					sum_salary += predict_rub_salary(vacancy['salary']['from'], vacancy['salary']['to'])
+					vacancies_processed += 1
 
-		if response.json()['found'] > 100:
-			page = 0
-			pages_number = 1
+		vacancy_info['vacancies_found'] = response_hh.json()['found']
+		vacancy_info['vacancies_processed'] = vacancies_processed
+		vacancy_info['average_salary'] = int(sum_salary/vacancies_processed)
 
-			vacancies = []
+		vacancies_for_language.update({language: vacancy_info})
 
-			while page < pages_number:
-				page_params = {
-					'text' : language,
-					'area' : 1, # Москва
-					'period' : 30,
-					'page': page
-				}
-
-				page_response = requests.get(vacancies_url, params=page_params)
-				page_response.raise_for_status()
-
-				pages_number = page_response.json()['pages']
-				page += 1
-	
-				vacancies.append(page_response.json()['items'])
-
-			vacancy_info['vacancies_found'] = response.json()['found']
-
-			for page in vacancies:
-				for vacancy in page:
-					if vacancy['salary'] and vacancy['salary']['currency'] == 'RUR':
-						sum_salary += predict_rub_salary_for_hh(vacancy)
-						vacancies_processed += 1
-
-			vacancy_info['vacancies_processed'] = vacancies_processed
-			vacancy_info['average_salary'] = int(sum_salary/vacancies_processed)
-
-			vacancies_for_language.update({language: vacancy_info})
 
 	display_table_of_result(vacancies_for_language, 'HeadHunter Moscow')
 
 
-def predict_rub_salary_for_hh(vacancy):
-	if vacancy['salary']['from'] and vacancy['salary']['to']:
-		return (vacancy['salary']['from']+vacancy['salary']['to'])/2
-	elif not vacancy['salary']['from'] and vacancy['salary']['to']:
-		return vacancy['salary']['to']*0.8
-	elif vacancy['salary']['from'] and not vacancy['salary']['to']:
-		return vacancy['salary']['from']*1.2
-
-
 if __name__ == '__main__':
 	show_hh_vacancies_info()
+	
--- show_vacancies_superjob.py
+++ show_vacancies_superjob.py
@@ -4,10 +4,50 @@ import requests
 from dotenv import load_dotenv
 
 from mapping_result import display_table_of_result
+from predict_rub_salary import predict_rub_salary
+
+def get_request_sj(language, url, headers):
+	
+	vacancies_params = {
+		'keyword': language,
+		'town': 'Москва'
+	}
+
+	response = requests.get(url, headers=headers, params=vacancies_params)
+	response.raise_for_status()
+
+	return response
+
+
+def download_pages(response, language, url, headers):
+	page = 0
+	vacancies = []
+
+	while page < 5: # Так как можно только 500 результатов
+		page_params = {
+			'keyword' : language,
+			'town' : 'Москва',
+			'page': page,
+			'count': 100
+		}
+
+		page_response = requests.get(url, headers=headers, params=page_params)
+		page_response.raise_for_status()
+
+		page_data = page_response.json()['objects']
+		page += 1
+	
+		vacancies.append(page_data)
+
+	return vacancies
 
 
 def show_sj_vacancies_info(api_key):
 	vacancies_url = 'https://api.superjob.ru/2.0/vacancies/'
+
+	headers = {
+		'X-Api-App-Id': api_key
+	}
 
 	languages_of_programming = [
 		'TypeScript',
@@ -34,60 +74,21 @@ def show_sj_vacancies_info(api_key):
 		vacancies_processed = 0
 		sum_salary = 0
 
-		headers = {
-			'X-Api-App-Id': api_key
-		}
-
-		vacancies_params = {
-			'keyword': language,
-			'town': 'Москва'
-		}
-
-		response = requests.get(vacancies_url, headers=headers, params=vacancies_params)
-		response.raise_for_status()
-
-		page = 0
-
-		vacancies = []
-
-		while page < 5: # Так как можно только 500 результатов
-			page_params = {
-				'keyword' : language,
-				'town' : 'Москва',
-				'page': page,
-				'count': 100
-			}
-
-			page_response = requests.get(vacancies_url, headers=headers, params=page_params)
-			page_response.raise_for_status()
-
-			page += 1
-	
-			vacancies.append(page_response.json()['objects'])
-
-		vacancy_info['vacancies_found'] = response.json()['total']
+		response_sj = get_request_sj(language, vacancies_url, headers)
+		vacancies = download_pages(response_sj, language, vacancies_url, headers)
 
 		for page in vacancies:
 			for vacancy in page:
 				if (vacancy['payment_from'] or vacancy['payment_to']) and vacancy['currency'] == 'rub':
-					sum_salary += predict_rub_salary_for_superjob(vacancy)
+					sum_salary += predict_rub_salary(vacancy['payment_from'], vacancy['payment_to'])
 					vacancies_processed += 1
 
+		vacancy_info['vacancies_found'] = response_sj.json()['total']
 		vacancy_info['vacancies_processed'] = vacancies_processed
 		vacancy_info['average_salary'] = int(sum_salary/vacancies_processed)
-
 		vacancies_for_language.update({language: vacancy_info})
-
+	
 	display_table_of_result(vacancies_for_language, 'SuperJob Moscow')
-
-
-def predict_rub_salary_for_superjob(vacancy):
-	if vacancy['payment_from'] and vacancy['payment_to']:
-		return (vacancy['payment_from']+vacancy['payment_to'])/2
-	elif not vacancy['payment_from'] and vacancy['payment_to']:
-		return vacancy['payment_to']*0.8
-	elif vacancy['payment_from'] and not vacancy['payment_to']:
-		return vacancy['payment_from']*1.2
 
 
 if __name__ == '__main__':
@@ -96,3 +97,4 @@ if __name__ == '__main__':
 	superjob_api = os.getenv('API_SUPERJOB')
 
 	show_sj_vacancies_info(superjob_api)
+		
