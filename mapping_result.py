2166f0da61d2ffbf2ecdd557efca6e253c7ba75f
PaSeRouS <paser96@mail.ru>
2022-02-18 20:09:37 +0300

    Избавление от табуляции

--- mapping_result.py
+++ mapping_result.py
@@ -2,19 +2,19 @@ from terminaltables import SingleTable
 
 
 def display_table_of_result(results, title):
-	table_data = [
-		['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
-	]
+    table_data = [
+	    ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
+    ]
 
-	for language in results:
-		language_result = []
-		language_result.append(language)
-		language_result.append(results[language]['vacancies_found'])
-		language_result.append(results[language]['vacancies_processed'])
-		language_result.append(results[language]['average_salary'])
+    for language in results:
+	    language_result = []
+	    language_result.append(language)
+	    language_result.append(results[language]['vacancies_found'])
+	    language_result.append(results[language]['vacancies_processed'])
+	    language_result.append(results[language]['average_salary'])
 
-		table_data.append(language_result)
+	    table_data.append(language_result)
 
-	table_instance = SingleTable(table_data, title)
-	print(table_instance.table)
-	print()
+    table_instance = SingleTable(table_data, title)
+    print(table_instance.table)
+    print()
